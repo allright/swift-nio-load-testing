29a1f1803c606eab2ea66c111cf46f3373899f26
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-03-06 13:39:30 +0300

    close task by timeout

--- swift-nio-echo-server/Sources/swift-nio-echo-server/EchoHandler.swift
+++ swift-nio-echo-server/Sources/swift-nio-echo-server/EchoHandler.swift
@@ -18,6 +18,8 @@ func log(_ s:String) {
 }
 
 private let handlersCount = Atomic<UInt32>(value:0)
+private let activeCount = Atomic<UInt32>(value:0)
+
 private let index = Atomic<UInt32>(value:0)
 
 internal final class EchoHandler: ChannelInboundHandler {
@@ -28,18 +30,34 @@ internal final class EchoHandler: ChannelInboundHandler {
 
     init() {
         _ = handlersCount.add(1)
-        log("EchoHandler:init   \(id):\(handlersCount.load())")
+        log("EchoHandler:init   \(id):\(handlersCount.load()):\(activeCount.load())")
+
     }
 
     deinit {
         _ = handlersCount.sub(1)
-        log("EchoHandler:deinit \(id):\(handlersCount.load())")
+        log("EchoHandler:deinit \(id):\(handlersCount.load()):\(activeCount.load())")
     }
 
     public func channelRead(ctx: ChannelHandlerContext, data: NIOAny) {
         // As we are not really interested getting notified on success or failure we just pass nil as promise to
         // reduce allocations.
         ctx.write(data, promise: nil)
+    }
+
+    func channelActive(ctx: ChannelHandlerContext) {
+        _ = activeCount.add(1)
+        log("EchoHandler:channelActive   \(id):\(handlersCount.load()):\(activeCount.load())")
+        ctx.eventLoop.scheduleTask(in: .seconds(30), { [weak self] in
+            guard let `self` = self else { return }
+            log("EchoHandler:scheduleTask done   \(self.id):\(handlersCount.load()):\(activeCount.load())")
+            ctx.close()
+        })
+    }
+
+    func channelInactive(ctx: ChannelHandlerContext) {
+        _ = activeCount.sub(1)
+        log("EchoHandler:channelActive   \(id):\(handlersCount.load()):\(activeCount.load())")
     }
 
     // Flush it out. This can make use of gathering writes if multiple buffers are pending
