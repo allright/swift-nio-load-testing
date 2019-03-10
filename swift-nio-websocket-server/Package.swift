f6cd43de3ffe38f4734c73f5108829c21da0c770
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-03-10 22:05:39 +0300

    swifth to nio-13.1 branch

--- swift-nio-websocket-server/Package.swift
+++ swift-nio-websocket-server/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-nio-websocket-server",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-nio.git", .branch("nio-1.13"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
