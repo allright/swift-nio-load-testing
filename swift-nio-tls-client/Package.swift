8c6b6b03205eee0c21a68660d3e2f8a3d4364caa
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-03-10 22:07:36 +0300

    switch to nio 1.13 branch everythere

--- swift-nio-tls-client/Package.swift
+++ swift-nio-tls-client/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-nio-tls-client",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", .branch("nio-1.13")),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0")
     ],
     targets: [
